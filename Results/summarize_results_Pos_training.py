0342a0e92687c210b1bf3a8c7b4491bad6127a74
adilsonmedronha <ad.medronha@gmail.com>
2025-04-17 18:17:07 -0300

    add: summarizer finetuning

---
+++ Results/summarize_results_Pos_training.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import os
+import argparse
+
+def generate_csv(exp_folder, exp_name):
+
+    main_path = f'{exp_folder}'
+    all_data = []
+
+    for folder_name in os.listdir(main_path):
+        folder_path = os.path.join(main_path, folder_name)
+
+        if os.path.isdir(folder_path):
+            for file_name in os.listdir(folder_path):
+                if file_name.endswith('.csv'):
+                    file_path = os.path.join(folder_path, file_name)
+                    df = pd.read_csv(file_path)
+                    
+                    avg_loss = df['avg loss'].mean().round(4)
+                    acc = df['acc'].mean().round(4)
+                    acc_std = df['acc'].std().round(4)
+                    f1 = df['f1'].mean().round(4)
+                    f1_std = df['f1'].std().round(4)
+                    recall = df['recall'].mean().round(4)
+                    recall_std = df['recall'].std().round(4)
+                    precision = df['precision'].mean().round(4)
+                    precision_std = df['precision'].std().round(4)
+                    model = df['model'].iloc[0]
+                    dataset = df['dataset'].iloc[0]
+
+                    avg_df = pd.DataFrame({
+                        'model': [model],
+                        'dataset': [dataset],
+                        'avg_loss': [avg_loss],
+                        'acc': [acc],
+                        'acc_std': [acc_std],
+                        'f1': [f1],
+                        'f1_std': [f1_std],
+                        'recall': [recall],
+                        'recall_std': [recall_std],
+                        'precision': [precision],
+                        'precision_std': [precision_std]
+                    })
+                    all_data.append(avg_df)
+
+    combined_df = pd.concat(all_data, ignore_index=True)
+    combined_df.sort_values(by='acc', ascending=False, inplace=True)
+    combined_df.to_csv(f'Pos_training/{exp_name}.csv', index=False)
+
+def main(exp_folder):
+    for folder_name in os.listdir(exp_folder):
+        folder_path = os.path.join(exp_folder, folder_name)
+        if os.path.isdir(folder_path):
+            generate_csv(folder_path, exp_name = folder_name)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Summarize CSVs in experiment folder.')
+    parser.add_argument('exp_folder', type=str, help='Name of the experiment folder inside Pos_training/')
+    args = parser.parse_args()
+    main(args.exp_folder)
