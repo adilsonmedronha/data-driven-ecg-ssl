8b8a94c531c769c4a244f7cdf602b083f4b0b86f
adilsonmedronha <ad.medronha@gmail.com>
2025-04-17 17:16:01 -0300

    add: finetuning and supervised

---
+++ models/heads/finetuning/MLP.py
@@ -0,0 +1,27 @@
+import torch 
+import torch.nn as nn
+
+
+class MLP(nn.Module):
+     def __init__(self, in_dim, hidden_dim, output_size):
+         super(MLP, self).__init__()
+         self.z1 = nn.Linear(in_dim, hidden_dim)
+         self.a1 = nn.ReLU()
+         self.z2 = nn.Linear(hidden_dim, output_size)
+         self.a2 = nn.ReLU()
+     
+         self.logits = nn.Sequential(
+             nn.Dropout(0.1),
+             nn.Linear(in_dim, hidden_dim),
+             nn.LeakyReLU(),
+             nn.Dropout(0.2),
+             nn.Linear(hidden_dim, hidden_dim),
+             nn.LeakyReLU(),
+             nn.Dropout(0.2),
+             nn.Linear(hidden_dim, output_size),
+             nn.LeakyReLU(),
+             nn.Dropout(0.3)
+         )
+ 
+     def forward(self, x):
+         return self.logits(x)
